c4753acf75895d929ff39e62a6f653fb7c2eae44
陈阳 <1337968347@qq.com>
2022-06-28 20:57:49 +0800

    :star: 添加斯坦福

---
+++ src/bunny-shader.js
@@ -0,0 +1,34 @@
+export const vertexShader = `
+uniform mat4 projection;
+uniform mat4 carMatrix;
+varying vec3 vNormal;
+varying vec3 VPosition;
+
+void main() {
+
+    vec4 worldPosition = mMatrix * vec4(position, 1.0);
+    gl_Position = projection * worldPosition;
+    VPosition = vec3(worldPosition.xyz);
+    vNormal = normalize((carMatrix * vec4(normal, 1.0) ).xyz);
+}
+
+`;
+export const fragmentShader = `
+precision highp float;
+uniform vec4 sunPos;
+uniform vec3 cameraPos;
+uniform vec4 sunColor;
+varying vec3 vNormal;
+varying vec3 VPosition;
+
+
+void main() {
+    vec2 pix = gl_FragCoord.xy / uResolution - vec2(0.5);
+    vec3 sunDirection = normalize(vec3(vec2(pix.x, -pix.y), -1.0));
+
+    vec3 finalColor = getSkyColor(sunDirection ,cameraPos) + vNormal;
+
+    gl_FragColor = vec4(finalColor, 1.0);
+
+}
+`;
--- src/index.js
+++ src/index.js
@@ -1,58 +1,61 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { vertexShader, fragmentShader } from './shadow-shader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { vertexShader, fragmentShader } from './sky-shader';
+import { bunnyStr } from './bunny-obj';
+import { getPosAndNormal } from './util';
 
-const loader = new GLTFLoader();
+const width = 1000;
+const height = 1000;
 
-loader.load(
-  './free_1972_datsun_240k_gt/scene.gltf',
-  function (gltf) {
-    const scene = new THREE.Scene();
-    const camera = new THREE.OrthographicCamera(-256, 256, 256, -256, 0, 256);
+const scene = new THREE.Scene();
+const camera = new THREE.OrthographicCamera(-256, 256, 256, -256, -256, 256);
 
-    camera.position.z = 10;
-    console.log(camera.position)
+camera.position.z = 10;
 
-    const renderer = new THREE.WebGLRenderer();
-    document.body.appendChild(renderer.domElement);
-    const geometry = new THREE.PlaneGeometry(1, 1);
+const renderer = new THREE.WebGLRenderer();
+document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-    const sunInfo = new THREE.Vector4(0, 150, 400, 20);
-    const uResolution = new THREE.Vector2(900, 900);
+const geometry = new THREE.PlaneGeometry(1, 1);
 
-    const worldMatrix = new THREE.Matrix4().makeScale(1000, 1000, 1000);
-    const carMatrix = new THREE.Matrix4().makeScale(10,10,10);
+// 斯坦福兔子
+const { position, normal } = getPosAndNormal(bunnyStr);
 
-    const material = new THREE.ShaderMaterial({
-      uniforms: {
-        cameraPos: { value: camera.position },
-        carMatrix: { value: carMatrix },
-        mMatrix: { value: worldMatrix },
-        projection: { value: camera.projectionMatrix },
-        sunInfo: { value: sunInfo },
-        uResolution: { value: uResolution },
-      },
-      vertexShader,
-      fragmentShader,
-    });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
 
-    const loop = (tick) => {
-      tick();
-      requestAnimationFrame(() => loop(tick));
-    };
+// 太阳的 x y z r
+const sunPos = new THREE.Vector4(0, 110, 300, 30);
+const sunColor = new THREE.Vector4(1.3, 1.0, 1.0, 1.0);
+const uResolution = new THREE.Vector2(width, height);
 
-    const render = () => {
-      renderer.setSize(uResolution.x, uResolution.y);
-      renderer.render(scene, camera);
-    };
+const worldMatrix = new THREE.Matrix4().makeScale(1000, 1000, 1000);
+const carMatrix = new THREE.Matrix4().makeScale(1000, 1000, 1000);
 
-    loop(render);
-    scene.add(gltf.scene);
+const material = new THREE.ShaderMaterial({
+  uniforms: {
+    cameraPos: { value: camera.position },
+    carMatrix: { value: carMatrix },
+    mMatrix: { value: worldMatrix },
+    projection: { value: camera.projectionMatrix },
+    sunPos: { value: sunPos },
+    sunColor: { value: sunColor },
+    uResolution: { value: uResolution },
   },
-  undefined,
-  function (error) {
-    console.error(error);
-  }
-);
+  vertexShader,
+  fragmentShader,
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+// scene.add(gltf.scene)
+
+const loop = (tick) => {
+  tick();
+  requestAnimationFrame(() => loop(tick));
+};
+
+const render = () => {
+  controls.update();
+  renderer.setSize(uResolution.x, uResolution.y);
+  renderer.render(scene, camera);
+};
+
+loop(render);
